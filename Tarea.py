b941f6a9c46a9ca68943aa4ce15d19744f64d381
Karen Livier Orozco Franco <141891718+LivMaster13@users.noreply.github.com>
2023-11-06 20:30:40 -0800

    Tarea python se me olvido que habia pido perdon

---
+++ Tarea.py
@@ -0,0 +1,24 @@
+# Este es un comentario que explica el propósito del script.
+
+def funcion_ejemplo(parametro1, parametro2):
+    """
+    Esta es la documentación de la función. Debe proporcionar una descripción
+    concisa de lo que hace la función, así como descripciones de los parámetros
+    y el valor de retorno, si los hubiera.
+
+    :param parametro1: Descripción del primer parámetro.
+    :param parametro2: Descripción del segundo parámetro.
+    :return: Descripción del valor de retorno, si lo hubiera.
+    """
+    resultado = parametro1 + parametro2
+    return resultado
+
+mi_variable = 42
+otra_variable = "Hola, mundo!"
+
+if mi_variable == 42:
+    print(otra_variable)
+else:
+    print("El valor de mi_variable no es 42.")
+
+    
